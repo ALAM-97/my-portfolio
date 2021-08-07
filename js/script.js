75d650c3c446a5fffb9d14e7459a274f983cd3d4
ALAM-97 <alexamara97@gmail.com>
2021-08-07 16:43:58 +0200

    FIX - Qualification Section

--- js/script.js
+++ js/script.js
@@ -6,9 +6,14 @@ function darkMode() {
 function mostraIstruzione() {
     document.getElementById("tab-istruzione").style.display="block";
     document.getElementById("tab-lavoro").style.display = "none";
+    document.getElementById("istruzione-active").style.opacity = "1";
+    document.getElementById("lavoro-active").style.opacity = "0.5";
 }
 
 function mostraLavoro() {
     document.getElementById("tab-lavoro").style.display = "block";
     document.getElementById("tab-istruzione").style.display = "none";
+    document.getElementById("lavoro-active").style.opacity = "1";
+    document.getElementById("istruzione-active").style.opacity = "0.5";
+
 }
