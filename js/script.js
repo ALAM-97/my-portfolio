54f5b3798f56e5954031f7f35d7df6957e053b48
ALAM-97 <alexamara97@gmail.com>
2021-08-07 13:58:43 +0200

    Qualification Section Added

--- js/script.js
+++ js/script.js
@@ -2,3 +2,13 @@ function darkMode() {
     var element = document.body;
     element.classList.toggle("dark-mode");
 }
+
+function mostraIstruzione() {
+    document.getElementById("tab-istruzione").style.display="block";
+    document.getElementById("tab-lavoro").style.display = "none";
+}
+
+function mostraLavoro() {
+    document.getElementById("tab-lavoro").style.display = "block";
+    document.getElementById("tab-istruzione").style.display = "none";
+}
